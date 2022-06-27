4545a672d60b4d355e3df63784b2c02764bc12a8
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-06-28 00:01:32 +0600

    basic setup

---
+++ src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-4xl'> 
+            Summer Art Camp! 5 Days of Artists and Painting Monet, Van Gogh, Matisse, & More
+            </h1>
+            <h1 className='text-4xl'> 
+                Multi-Day Course <span>?</span>
+            </h1>
+        </div>
+    );
+};
+
+export default Home;
